ffc2f5b6eba0feaa8d2af2e1715d69bf0d72e2fd
gavril_alexandru <80162292+gavalexandru@users.noreply.github.com>
2025-06-02 00:07:45 +0300

    Expiration time changed

--- Backend/Server/src/main/java/org/example/JWT.java
+++ Backend/Server/src/main/java/org/example/JWT.java
@@ -6,12 +6,12 @@ import java.util.Date;
 
 public class JWT {
     private static final String secretKey = "7A24432646294A404E635266556A586E3272357538782F413F4428472B4B6150";
-    private static final long expirationTime = 86400000; //24h in miliseconds
+    private static final long expirationTime = 900000; //15m in miliseconds
 
-    public static String generateToken(String username) {
+    public static String generateToken(String email) {
         SecretKey key = Keys.hmacShaKeyFor(secretKey.getBytes());
         return Jwts.builder()
-                .subject(username)
+                .subject(email)
                 .expiration(new Date(System.currentTimeMillis() + expirationTime))
                 .signWith(key)
                 .compact();
