fcc0f6388c4c9236a1fcdb90c3057d91c91ded68
gavril_alexandru <80162292+gavalexandru@users.noreply.github.com>
2025-06-02 00:06:41 +0300

    Configure login response

--- Backend/Server/src/main/java/org/example/HttpResponse.java
+++ Backend/Server/src/main/java/org/example/HttpResponse.java
@@ -5,24 +5,48 @@ public class HttpResponse {
     private final String statusMessage;
     private final String contentType;
     private final String content;
-
-    public HttpResponse(int statusCode, String statusMessage, String contentType, String content) {
+    private final String session;
+    private final String jwt;
+    public HttpResponse(int statusCode, String statusMessage, String contentType, String content, String session, String jwt) {
         this.statusCode = statusCode;
         this.statusMessage = statusMessage;
         this.contentType = contentType;
         this.content = content;
+        this.session = session;
+        this.jwt = jwt;
     }
 
     public String getHeaders() {
-        return "HTTP/1.1 " + statusCode + " " + statusMessage + "\r\n" +
-                "Content-Type: " + contentType + "\r\n" +
-                "Content-Length: " + content.length() + "\r\n" +
-                "Access-Control-Allow-Origin: http://localhost\r\n" +
-                "Access-Control-Allow-Methods: GET, POST, OPTIONS\r\n" +
-                "Access-Control-Allow-Headers: Content-Type\r\n" +
-                "Access-Control-Allow-Credentials: true\r\n" +
-                "Connection: close\r\n" +
-                "\r\n";
+        System.out.println("jwt: " + jwt);
+        if(session == "/login" && jwt != null){
+            return "HTTP/1.1 " + statusCode + " " + statusMessage + "\r\n" +
+                    "Content-Type: " + contentType + "\r\n" +
+                    "Content-Length: " + content.length() + "\r\n" +
+                    "Set-Cookie: jwt=" + jwt + "; HttpOnly; Path=/; Max-Age=900\r\n" +
+                    "Cache-Control: no-cache, no-store, must-revalidate\r\n" +
+                    "Pragma: no-cache\r\n" +
+                    "Expires: 0\r\n" +
+                    "X-Content-Type-Options: nosniff\r\n" +
+                    "X-Frame-Options: DENY\r\n" +
+                    "X-XSS-Protection: 1; mode=block\r\n" +
+                    "Access-Control-Allow-Origin: http://localhost\r\n" +
+                    "Access-Control-Allow-Methods: GET, POST, OPTIONS\r\n" +
+                    "Access-Control-Allow-Headers: Content-Type\r\n" +
+                    "Access-Control-Allow-Credentials: true\r\n" +
+                    "Connection: close\r\n" +
+                    "\r\n";
+        }
+        else {
+            return "HTTP/1.1 " + statusCode + " " + statusMessage + "\r\n" +
+                    "Content-Type: " + contentType + "\r\n" +
+                    "Content-Length: " + content.length() + "\r\n" +
+                    "Access-Control-Allow-Origin: http://localhost\r\n" +
+                    "Access-Control-Allow-Methods: GET, POST, OPTIONS\r\n" +
+                    "Access-Control-Allow-Headers: Content-Type\r\n" +
+                    "Access-Control-Allow-Credentials: true\r\n" +
+                    "Connection: close\r\n" +
+                    "\r\n";
+        }
     }
 
     public String getContent() {
