a9475a44bbdd243cf92f272be4773406170b7e92
gavril_alexandru <80162292+gavalexandru@users.noreply.github.com>
2025-06-01 19:51:55 +0300

    Json Web Token - validator and generator

---
+++ Backend/Server/src/main/java/org/example/JWT.java
@@ -0,0 +1,31 @@
+package org.example;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+public class JWT {
+    private static final String secretKey = "7A24432646294A404E635266556A586E3272357538782F413F4428472B4B6150";
+    private static final long expirationTime = 86400000; //24h in miliseconds
+
+    public static String generateToken(String username) {
+        SecretKey key = Keys.hmacShaKeyFor(secretKey.getBytes());
+        return Jwts.builder()
+                .subject(username)
+                .expiration(new Date(System.currentTimeMillis() + expirationTime))
+                .signWith(key)
+                .compact();
+    }
+
+    public static boolean validateToken(String token) {
+        try {
+            SecretKey key = Keys.hmacShaKeyFor(secretKey.getBytes());
+            Jwts.parser().verifyWith(key).build().parseSignedClaims(token);
+            return true;
+        }
+        catch (Exception e) {
+            return false;
+        }
+    }
+
+}
