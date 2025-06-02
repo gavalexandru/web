d91cac8052c5dce0e37df9b91deb729821846436
gavril_alexandru <80162292+gavalexandru@users.noreply.github.com>
2025-06-03 00:33:42 +0300

    Generating refresh token for Login and getting email from JWT

--- Backend/Server/src/main/java/org/example/JWT.java
+++ Backend/Server/src/main/java/org/example/JWT.java
@@ -1,4 +1,5 @@
 package org.example;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import javax.crypto.SecretKey;
@@ -6,13 +7,23 @@ import java.util.Date;
 
 public class JWT {
     private static final String secretKey = "7A24432646294A404E635266556A586E3272357538782F413F4428472B4B6150";
-    private static final long expirationTime = 900000; //15m in miliseconds
+    private static final long expirationTime = 900000; //15 minutes in miliseconds for access_token
+    private static final long longerExpiration = 604800000; //7 days in miliseconds for refresh_token
 
-    public static String generateToken(String email) {
+    public static String generateAccessToken(String email) {
         SecretKey key = Keys.hmacShaKeyFor(secretKey.getBytes());
         return Jwts.builder()
                 .subject(email)
                 .expiration(new Date(System.currentTimeMillis() + expirationTime))
+                .signWith(key)
+                .compact();
+    }
+
+    public static String generateRefreshToken(String email) {
+        SecretKey key = Keys.hmacShaKeyFor(secretKey.getBytes());
+        return Jwts.builder()
+                .subject(email)
+                .expiration(new Date(System.currentTimeMillis() + longerExpiration))
                 .signWith(key)
                 .compact();
     }
@@ -28,4 +39,9 @@ public class JWT {
         }
     }
 
+    public static String getEmailFromToken(String token) {
+        SecretKey key = Keys.hmacShaKeyFor(secretKey.getBytes());
+        Claims claims = Jwts.parser().setSigningKey(key).build().parseClaimsJwt(token).getBody();
+        return claims.get("email", String.class);
+    }
 }
