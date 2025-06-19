0f8c18ff767a327f1ab20efd88294a89a6579c9e
gavril_alexandru <80162292+gavalexandru@users.noreply.github.com>
2025-06-19 19:12:36 +0300

    Delete Database directory

--- Database/logged.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE logged (
-   user_id INTEGER REFERENCES users(user_id),
-   access_token VARCHAR(512) NULL,
-   refresh_token VARCHAR(512)
-);
--- Database/users.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE users (
-    user_id SERIAL PRIMARY KEY,
-    email VARCHAR(100),
-    password VARCHAR(100)
-);
