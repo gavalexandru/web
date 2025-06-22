4597136b39af6e33242e8fb65df79f8d9e3d3e7a
gavril_alexandru <80162292+gavalexandru@users.noreply.github.com>
2025-06-23 01:58:01 +0300

    Delete server/database directory - wrong place

--- server/database/database schema/revoked_tokens.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE revoked_tokens (
-    id SERIAL PRIMARY KEY,
-    token TEXT UNIQUE NOT NULL,
-    user_id INTEGER NOT NULL REFERENCES users(user_id) ON DELETE CASCADE
-);
--- server/database/database schema/users.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE users (
-    user_id SERIAL PRIMARY KEY,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    password VARCHAR(100) NOT NULL
-);
